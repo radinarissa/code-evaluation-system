ea38e701e3939bbbbb83749c6922a2c734810f57
StoilStoilov4 <sstoilov2004@gmail.com>
2025-12-01 20:43:52 +0000

    Initial version.php file for the moodle plugin configuration.

---
+++ moodle-plugin/assignsubmission_codeeval/lang/en/assignsubmission_codeeval.php
@@ -0,0 +1,3 @@
+<?php
+$string['pluginname'] = 'CodeEval submission';
+$string['privacy:metadata'] = 'The CodeEval submission plugin does not store any personal data.';
---
+++ moodle-plugin/assignsubmission_codeeval/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'assignsubmission_codeeval';
+$plugin->version   = 2025111801;   // plugin version
+$plugin->requires  = 2019111800;   // Moodle 3.8 (MOODLE_38_STABLE)
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '0.1';
