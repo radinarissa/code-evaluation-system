ec29d969158a6d492e1c899870510105f8fab22f
Deyan Azmi <djdeijan@gmail.com>
2025-12-09 10:26:40 +0200

    Removed the webApp folder. The .NET web app is now in the backend folder.

--- webApp/CodeEvaluationSystem/CodeEvaluationSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CodeEvaluationSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
