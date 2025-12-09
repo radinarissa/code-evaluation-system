1bb031b99258466fbfff0dd0cebe7a5fb583cf4c
StoilStoilov4 <sstoilov2004@gmail.com>
2025-12-09 21:15:27 +0000

    Fix plugin symlink logic. Add a mock version of the plguin for testing.

---
+++ moodle-plugin/assignsubmission_codeeval/lang/en/assignsubmission_codeeval.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname'] = 'CodeEval submission';
+$string['sourcecode'] = 'Source code';
+$string['summarychars'] = 'Submitted code ({$a} characters)';
+$string['nosubmission'] = 'No code submitted yet';
+$string['privacy:metadata'] = 'The CodeEval submission plugin does not store any personal data.';
