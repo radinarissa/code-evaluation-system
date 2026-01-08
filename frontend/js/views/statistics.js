8911ef150ec461990898d2cc9b73e3ab2b57818e
Deyan Azmi <djdeijan@gmail.com>
2026-01-08 03:09:36 +0200

    Update statistics.js

--- frontend/js/views/statistics.js
+++ frontend/js/views/statistics.js
@@ -185,10 +185,10 @@ const StatisticsView = {
         if (!this.taskId)
         {
             let allTasks = await ApiService.getTasks();
-
+            console.log(allTasks);
             this.taskId = allTasks.length > 0 ? allTasks[0].id : 0;
         }
-        
+
         const [submissions, tasks, task] = await Promise.all([
             ApiService.getSubmissionsByTaskId(this.taskId),
             ApiService.getTasks(),
